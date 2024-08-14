014b5498b67d3de483c141d5e772dbf4e57e12b5
Jeff Leung <badjeff@gmail.com>
2024-08-14 21:31:34 +0800

    use separated slot index in reserve_or_peripheral_slot_for_conn()

--- src/output_relay_central.c
+++ src/output_relay_central.c
@@ -91,6 +91,17 @@ static int release_or_peripheral_slot(int index) {
 }
 
 static int reserve_or_peripheral_slot_for_conn(struct bt_conn *conn) {
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_PREF_WEAK_BOND)
+    for (int i = 0; i < ZMK_SPLIT_BLE_PERIPHERAL_COUNT; i++) {
+        if (peripherals[i].state == PERIPHERAL_SLOT_STATE_OPEN) {
+            // Be sure the slot is fully reinitialized.
+            release_or_peripheral_slot(i);
+            peripherals[i].conn = conn;
+            peripherals[i].state = PERIPHERAL_SLOT_STATE_CONNECTED;
+            return i;
+        }
+    }
+#else
     int i = zmk_ble_put_peripheral_addr(bt_conn_get_dst(conn));
     if (i >= 0) {
         if (peripherals[i].state == PERIPHERAL_SLOT_STATE_OPEN) {
@@ -101,6 +112,7 @@ static int reserve_or_peripheral_slot_for_conn(struct bt_conn *conn) {
             return i;
         }
     }
+#endif // IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_PREF_WEAK_BOND)
 
     return -ENOMEM;
 }
