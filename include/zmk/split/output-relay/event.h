a666fe765c085ec4e0725e58b11758794d5b6a62
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-07-25 23:21:40 +0200

    Update event.h

--- include/zmk/split/output-relay/event.h
+++ include/zmk/split/output-relay/event.h
@@ -6,26 +6,20 @@
 
 #pragma once
 
+#define MAX_PAYLOAD_LEN 32
+
 struct zmk_split_bt_output_relay_event {
     uint8_t relay_channel;
     uint8_t value;
-
-    //** TODO: send payload_size & payload, only if size > 0
-
     uint8_t payload_size;
-    void *payload;
-
+    uint8_t payload[MAX_PAYLOAD_LEN];
 } __packed;
 
 struct zmk_split_output_event {
     const struct device *dev;
     uint8_t value;
-
-    //** TODO: send payload_size & payload, only if size > 0
-
-    uint8_t payload_size;
-    void *payload;
-
+    uint8_t payload_len;
+    uint8_t payload[MAX_PAYLOAD_LEN];
 };
 
 int zmk_split_bt_invoke_output(const struct device *dev, 
